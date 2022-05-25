f958fef5ad4616c173ae5ea9befe08506a1501c5
sephraghaei <105919260+sephraghaei@users.noreply.github.com>
2022-05-25 19:48:34 +0430

    Add files via upload

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Obstacle")
+        {
+            movement.enabled = false;
+
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
